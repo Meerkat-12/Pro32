273cdad677b008cf2c95bf476f9b9cfd2522ce85
Meerkat-12 <78534086+Meerkat-12@users.noreply.github.com>
2021-05-21 15:35:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var bg="sunrise1.png";
+
+
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundImg();
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+    background(bg);
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+getBackgroundImg();
+
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
+
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,16);
+    
+    if(hour>=04 && hour<=06){
+        bg="sunrise1.png";
+    }
+    else if(hour>=06 && hour<=08){
+        bg="sunrise2.png";
+    }
+    else if(hour>=23&&hour==0){
+        bg="sunset10.png";
+    }
+    else if(hour==0&&hour<=03){
+        bg="sunrise4.png";
+    }
+    else if(hour>=012&&hour<=014){
+        bg="sunset11.png";
+    }
+    else{
+        bg = "sunset12.png";
+    }
+    
+    
+
+   
+    console.log(hour);
+}
